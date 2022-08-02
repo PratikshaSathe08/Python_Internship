88e324bb5dfc7688216e1827b8eb1af9c03ba4f9
PratikshaSathe08 <pratikshasathe9689@gmail.com>
2022-08-02 15:24:44 +0530

    python_Programs

--- Day4/Global_Variable.py
+++ Day4/Global_Variable.py
@@ -1,18 +1,13 @@
-Name = "Shiv"
-#
-'''def myfunc():
-  print("My name is " + name)
- 
- name="abc"
- print=(name)
-myfunc()
-'''
+Name = "Pratiksha"
+def myfunc():                       #function 1                   
+ print(Name)
+myfunc()                            #call function 1
 
-def myfunc_2():
-    global name
-    name="abc"
-    print(name)
+def myfunc_2():                     #function 1                   
+    global Name
+    Name="Piya"
+    print(Name)
 
-myfunc_2()
+myfunc_2()                          #call function 1  
 
-print("my name is"+name)
+print("my name is "+Name)
