f474f5866becdeea58ec2897c6dc9fb7d5f30a41
PratikshaSathe08 <pratikshasathe9689@gmail.com>
2022-08-13 14:01:31 +0530

    Python_programs

---
+++ Day5/datatypes.py
@@ -0,0 +1,64 @@
+name="Pratiksha"
+print(name)
+x=range(10)
+print(x)
+print(type(x))
+
+dict = {"name" : "shiv", "age" : 25}
+print(type(dict))
+
+fruitSet= {"apple", "pear", "grapes"}
+print(type(fruitSet))
+
+fruitFrozenSet= ({"apple", "pear", "grapes"})
+print(type(fruitFrozenSet))
+
+boolValue = True
+print(type(boolValue))
+
+Bytes = b"this is bytes"
+print(type(Bytes))
+
+byteArray = bytearray(10)
+print(type(byteArray))
+
+NoneType=None
+print(type(NoneType))
+
+tuple = ("apple", "pear", "grapes")
+print(type(tuple))
+
+fruitList=["apple", "pear", "grapes"]
+print(type(fruitList))
+
+complexNumber = 10j
+print(type(complexNumber))
+
+floatNumber=10.5
+print(type(floatNumber))
+
+integerNumber=10
+print(type(integerNumber))
+
+#setting a specific datatpe
+name=str("10")
+print(type(name))
+
+int(100)
+
+float(20.5)
+complex=complex(100j)
+
+fruitList = list(("apple", "pear", "grapes"))
+
+fruitTuple= tuple(("apple", "pear", "grapes"))
+
+ranges=range(10)
+
+dictionary = dict(name="shiv", age=25)
+
+fruitSet = set(("apple", "pear", "grapes"))
+
+fruitFrozenSet = frozenset(("apple", "banana", "cherry"))
+
+boolType= bool(false)
---
+++ Day5/string.py
@@ -0,0 +1,28 @@
+string="hello i am pratiksha and i as software engineer"
+print(string)
+
+                  
+print(len(string))                       #len
+
+print("software" in string)              #in
+string2="12234657287387"
+print("252"in string2)
+
+result="angular"not in string           #not in
+print(result)
+
+
+print(string[0:5])                      #string slicing
+print(string[:21])                      #string slicing from start
+print(string[2:])                      #string slicing to end
+
+print(string[-10:-2])                  #negative slicing
+
+'''Positive Indexing
+E n g i n e e r
+0 1 2 3 4 5 6 7'''
+
+'''Negative Indexing
+E   n  g  i  n  e  e  r
+-8 -7 -6 -5 -4 -3 -2 -1
+'''
